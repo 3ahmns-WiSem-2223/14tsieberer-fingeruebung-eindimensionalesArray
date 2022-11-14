471c18718dce36cac28e5280db3e5812adf18977
Tobias Sieberer <114923448+tsieberer@users.noreply.github.com>
2022-11-14 16:06:40 +0100

    Abschnitt 2 Aufgabe Fingeruebung
    Deklaration von array1, array2, array3. array4 und deren Initialiserung auf 3 unterschiedliche Arten:
    
    array1 Datentyp int, 4 Elemente
    
    array2 Datentyp int, 4 Elemente, Werte 21,22,23,24
    
    array3 Datentyp int, 7 Elemente, Werte 31-37
    
    array4 Datentyp string, 10 Elemente, Werte "array4"
    arrayColoredImagesRed Datentyp GameObject mittels GameObject.FindGameObjectsWithTag("RedImage"); dabei ist natürlich
    Voraussetzung, dass die Images im 2D Unity Projekt angelegt sind (4 Elemente) und das Tag RedImage jeweils ausgewählt ist.

--- Assets/MyGame/Scripts/FingeruebungScript.cs
+++ Assets/MyGame/Scripts/FingeruebungScript.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class FingeruebungScript : MonoBehaviour
 {
+    //Deklaration eines Arrays namens array vom Datentyp int
     int[] array;
+    //Deklaration von array1, array2, array3. array4 und deren Initialiserung auf 3 unterschiedliche Arten:
+    int[] array1 = new int[4];
+    int[] array2 = new int[4] { 21, 22, 23, 24 };
+    int[] array3 = new int[] { 31, 32, 33,34,35,36,37};
+    string[] array4 = new string[10];
+    public GameObject[] arrayColoredImagesRed;
+    private void Start()
+    {
+        for (int i = 0; i < array4.Length; i++)
+        {
+            array4[i] = "array4";
+        }
+
+        arrayColoredImagesRed = GameObject.FindGameObjectsWithTag("RedImage");
+    }
 }
