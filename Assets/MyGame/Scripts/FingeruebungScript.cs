59d01cbacc79d3f8ee965125fde4451c0abaaa2f
Tobias Sieberer <114923448+tsieberer@users.noreply.github.com>
2022-11-21 19:47:25 +0100

    Abschnitt 6 Aufgabe Fingeruebung
    Weitere Beispiele:
    
    Erstelle ein weiteres Array mit Namen array5, dass mit allen geraden Zahlen zwischen 20 und 70 gefüllt ist.
    
    Gefordert ist ein Array mit Namen array6, dass alle geraden Zahlen zwischen 0 und 500 enthält.
    
    Gefordert ist ein Array mit Namen array7, dass alle ungeraden Zahlen zwischen 0 und 500 enthält.
    
    Gefordert ist ein Array mit Namen array8, dass 100 Elemente hält und als Elemente folgende Buchstabenreihe hat: "a", "b", "a", "b","a", "b","a", "b", ...
    
    Gefordert ist ein Array mit Namen array9, mit 1000 Elementen. Die Elemente werden mit Randomzahlen gefüllt und ausgegeben über Debug.
    
    Das array9 einhält unsortierte int Werte. Sortiere das Array und gib mittels Debug.Log die Werte des sortierten array9 aus. HINT: Hier wird System.Array.Sort() verwendet!!  
    
    Erweitern des Array durch zusammenkopieren von 2 Arrays. Kopiere array5 und arry6 in ein neues array10
    
    Gefordert ist ein Array mit Namen array11, mit 1000 Elementen. Die Elemente werden mit Randomzahlen gefüllt, ermittle das Minimum und Maximum der enthaltenen Werte.
    
    Gefordert ist ein Array mit Namen array12, mit 1000 Elementen. Die Elemente werden mit Randomzahlen gefüllt. Suche einen Wert und gib den Index retour, Hint: System.Array nachschauen

--- Assets/MyGame/Scripts/FingeruebungScript.cs
+++ Assets/MyGame/Scripts/FingeruebungScript.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +9,26 @@ public class FingeruebungScript : MonoBehaviour
     //Deklaration von array1, array2, array3. array4 und deren Initialiserung auf 3 unterschiedliche Arten:
     int[] array1 = new int[4];
     int[] array2 = new int[4] { 21, 22, 23, 24 };
-    int[] array3 = new int[] { 31, 32, 33,34,35,36,37};
+    int[] array3 = new int[] { 31, 32, 33, 34, 35, 36, 37 };
     string[] array4 = new string[10];
-    public GameObject[] arrayColoredImagesRed;
+    //Erstelle ein weiteres Array mit Namen array5, dass mit allen geraden Zahlen zwischen 20 und 70 gefüllt ist
+    int[] array5 = new int[26];
+    //Gefordert ist ein Array mit Namen array6, dass alle geraden Zahlen zwischen 0 und 500 enthält
+    int[] array6 = new int[251];
+    //Gefordert ist ein Array mit Namen array7, dass alle ungeraden Zahlen zwischen 0 und 500 enthält
+    int[] array7 = new int[250];
+    //Gefordert ist ein Array mit Namen array8, dass 100 Elemente hält und als Elemente folgende Buchstabenreihe hat: "a", "b", "a", "b","a", "b","a", "b", ...
+    string[] array8 = new string[100];
+    //Gefordert ist ein Array mit Namen array9, mit 1000 Elementen.Die Elemente werden mit Randomzahlen gefüllt und ausgegeben über Debug.
+    float[] array9 = new float[1000];
+    //Erweitern des Array durch zusammenkopieren von 2 Arrays. Kopiere array5 und arry6 in ein neues array10
+    int[] array10;
+    //Gefordert ist ein Array mit Namen array11, mit 1000 Elementen. Die Elemente werden mit Randomzahlen gefüllt, ermittle das Minimum und Maximum der enthaltenen Werte.
+    float[] array11 = new float[1000];
+    //Gefordert ist ein Array mit Namen array12, mit 1000 Elementen. Die Elemente werden mit Randomzahlen gefüllt. Suche einen Wert und gib den Index retour, Hint: System.Array nachschauen!
+    int[] array12 = new int[1000];
+
+    GameObject[] arrayColoredImagesRed;
     private void Start()
     {
         for (int i = 0; i < array4.Length; i++)
@@ -25,6 +43,7 @@ public class FingeruebungScript : MonoBehaviour
         WertMitIndex();
         WertAlsString();
         EinzelneElemente();
+        NurGewisseZahlen();
     }
 
     // 1) auf erstes Element -- Jeweils ein Debug.Log ausgeben. Beim arrayColoredImageRed den Namen des Objektes ausgeben.
@@ -85,5 +104,80 @@ public class FingeruebungScript : MonoBehaviour
         Debug.Log(array4[2]);
         //Greifen Sie auf das 3te Element in arrayColoredImagesRed zu und ändern sie die Farbe auf grün. Die Änderung sollte in der Scene/Game View ersichtlich sein
         arrayColoredImagesRed[2].GetComponent<Image>().color = Color.green;
+    }
+
+    //Weitere Beispiele:
+    void NurGewisseZahlen()
+    {
+        for(int i = 0; i < array5.Length; i++)
+        {
+            array5[i] = 20 + 2* i;
+        }
+
+        for (int i = 0; i < array6.Length; i++)
+        {
+            array6[i] = 2 * i;
+        }
+
+        for (int i = 0; i < array7.Length; i++)
+        {
+            array7[i] = 2 * i + 1;
+        }
+
+        for (int i = 0; i < array8.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                array8[i] = "a";
+            }
+            else
+            {
+                array8[i] = "b";
+            }
+        }
+
+        for (int i = 0; i < array9.Length; i++)
+        {
+            array9[i] = Random.value;
+            Debug.Log("array9 Wert an Stelle " + i + " entspricht: " + array9[i]);
+        }
+
+        System.Array.Sort(array9);
+        for (int i = 0; i < array9.Length; i++)
+        {
+            Debug.Log("Sortierter array9 Wert an Stelle " + i + " entspricht: " + array9[i]);
+        }
+
+        array10 = array5.Concat(array6).ToArray();
+
+        for (int i = 0; i < array11.Length; i++)
+        {
+            array11[i] = Random.value;
+        }
+
+        Debug.Log("array11 höchster Wert ist: " + array11.Max());
+        Debug.Log("array11 niedrigster Wert ist: " + array11.Min());
+
+        for(int i = 0; i < array12.Length; i++)
+        {
+            array12[i] = Random.Range(0, 10);
+        }
+        IndexOfNumber();
+    }
+
+    private int IndexOfNumber()
+    {
+        int indexOfNumber = 0;
+
+        indexOfNumber = System.Array.IndexOf(array12, 7, 0);
+        if (indexOfNumber >= 0)
+        {
+            Debug.Log("Die Zah 7 kommt zuerst bei Index " + indexOfNumber + " vor");
+        }
+        else
+        {
+            Debug.Log("Die Zahl 7 kommt in keiner der 1000 Stellen vor");
+        }
+        return indexOfNumber;
     }
 }
